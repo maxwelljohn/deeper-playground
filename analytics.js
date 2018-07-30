199683e22a43c631cbe3f6d264477522ba4517b2
John Maxwell <dreamalgebra@gmail.com>
2018-07-30 19:23:24 +0100

    Update analytics ID

--- analytics.js
+++ analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-var ANALYTICS_ID = 'Add your own analytics ID here';
+var ANALYTICS_ID = 'UA-15654273-3';
 ga('create', ANALYTICS_ID, 'auto');
